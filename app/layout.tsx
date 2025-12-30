2669564774c49f0560b5ee66617c22bad1d3fa69
Quang Minh <minhqnd@gmail.com>
2025-12-31 00:12:41 +0700

    feat: Add Vercel Analytics dependency and integration.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import Script from 'next/script';
 import './globals.css';
+import { Analytics } from "@vercel/analytics/next"
 
 const GA_MEASUREMENT_ID = 'G-YJKGC095KG';
 
@@ -64,6 +65,7 @@ export default function DictionaryLayout({
         <html lang="vi">
             <head>
                 {/* Google Analytics */}
+                <Analytics/>
                 <Script
                     src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
                     strategy="afterInteractive"

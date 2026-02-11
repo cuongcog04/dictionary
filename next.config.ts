032ee960146dd1791bdebfcd3162604ae7cd9a55
cuongcog04 <162123751+cuongcog04@users.noreply.github.com>
2026-02-11 12:10:00 +0700

    Add CORS headers for response handling

--- next.config.ts
+++ next.config.ts
@@ -32,6 +32,8 @@ const nextConfig: NextConfig = {
           { key: "Access-Control-Allow-Origin", value: "*" }, 
           { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
           { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+          { key: "Access-Control-Expose-Headers", value: "Content-Length, Content-Range" },
+          { key: "Accept-Ranges", value: "bytes" }
         ]
       },
     ];
